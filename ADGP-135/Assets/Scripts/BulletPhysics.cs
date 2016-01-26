f76bcf4758a54880398eef5be009d428f8df21f9
stevevice <steve_alexanders@bellsouth.net>
2016-01-26 17:54:37 -0600

    bullet program

---
+++ ADGP-135/Assets/Scripts/BulletPhysics.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletPhysics : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = transform.up * speed; 
+    }
+}
