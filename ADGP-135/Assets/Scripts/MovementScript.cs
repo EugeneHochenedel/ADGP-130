a93d57c80a94d850746092f612d2b4b870107f4a
stevevice <steve_alexanders@bellsouth.net>
2016-01-26 15:05:46 -0600

    Scripts folder and movement with start on roll

---
+++ ADGP-135/Assets/Scripts/MovementScript.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementScript : MonoBehaviour
+{
+    public float speed = 1f
+        ;
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(speed * 4 * Time.deltaTime, 0.0f, 0.0f);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(speed * -4 * Time.deltaTime, 0.0f, 0.0f);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0.0f, 0.0f, speed * 4 * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += new Vector3(0.0f, 0.0f, speed * -4 * Time.deltaTime);
+        }
+        
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.position += new Vector3(0.0f, speed * 2 * Time.deltaTime, 0.0f);
+            transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.position += new Vector3(0.0f, speed * -2 * Time.deltaTime, 0.0f);
+            transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+        }
+    }
+}
+
