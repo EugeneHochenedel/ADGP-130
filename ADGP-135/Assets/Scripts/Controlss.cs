6349150053e71ecd742d3af93ca89782b896b2fe
SilentTremor <eugeneatschool@gmail.com>
2016-02-02 12:05:53 -0600

    Testing
    Testing how Roll affects camera controls and movement

--- ADGP-135/Assets/Scripts/Controlss.cs
+++ ADGP-135/Assets/Scripts/Controlss.cs
@@ -7,27 +7,30 @@ public class Controlss : MonoBehaviour {
 	public float AimSensitivity;
 
 	float Pitch = 0.0f;
+	float Roll = 0.0f;
 	//public float PitchRange;
 
 	// Use this for initialization
 	void Start () {
 		Cursor.visible = false;
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
+	void Update() {
+		Roll = Input.GetAxis("Diagonal") * AimSensitivity;
+		Camera.main.transform.localRotation = Quaternion.Euler(0, 0, Roll);
 
 		float Yaw = Input.GetAxis("Mouse X") * AimSensitivity;
-		transform.Rotate(0, Yaw, 0);
+		Camera.main.transform.Rotate(0, Yaw, Roll);
 
 		Pitch -= Input.GetAxis("Mouse Y") * AimSensitivity;
-		
-		Camera.main.transform.localRotation = Quaternion.Euler(Pitch, 0, 0);
+		Camera.main.transform.localRotation = Quaternion.Euler(Pitch, 0, Roll);
 
 		float LongitudinalSpeed = Input.GetAxis("Vertical") * Velocity;
 		float LateralSpeed = Input.GetAxis("Horizontal") * Velocity;
 		float VerticalSpeed = Input.GetAxis("Jump") * Velocity;
 
+		
 		Vector3 speed = new Vector3(LateralSpeed, VerticalSpeed, LongitudinalSpeed);
 		speed = transform.rotation * speed;
 
