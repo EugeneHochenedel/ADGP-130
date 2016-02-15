a7c0f4f9ebff65c8d543142781d9b90bd70dc375
stevevice <steve_alexanders@bellsouth.net>
2016-02-15 16:57:14 -0600

    Practice  for Menu's

--- ADGP-135/Assets/Scripts/Exit.cs
+++ ADGP-135/Assets/Scripts/Exit.cs
@@ -4,13 +4,9 @@ using System.Collections;
 
 public class Exit : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update()
+    {
+        Application.Quit();
 	}
 }
