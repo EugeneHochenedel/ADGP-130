2691a3af69440d22d5621fcb4581cee3f40cbce8
stevevice <steve_alexanders@bellsouth.net>
2016-02-16 16:26:04 -0600

    Trying to get things to work

---
+++ ADGP-135/Assets/Scripts/Enemy1Ctrl.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy1Ctrl : MonoBehaviour
+{
+    public GameObject Bullet;
+    public float speed;
+
+    void Shoot()
+    {
+  
+        GameObject B = Instantiate(Bullet);
+
+        B.transform.position = gameObject.transform.position;
+
+        B.GetComponent<Rigidbody>().AddForce(gameObject.transform.forward * speed);
+    }
+
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }
+    }
+
+}
---
+++ ADGP-135/Assets/Scripts/LoadLevelTest.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadLevelTest : MonoBehaviour
+{
+
+    public void Load(string lvl)
+    {
+        SceneManager.LoadScene(lvl);
+    }
+}
