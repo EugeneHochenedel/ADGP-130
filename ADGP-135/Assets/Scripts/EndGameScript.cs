eb93b4b5a9db7802bb328df6ac71d18a17037b15
stevevice <steve_alexanders@bellsouth.net>
2016-02-23 16:12:44 -0600

    EndGame

---
+++ ADGP-135/Assets/Scripts/EndGameScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class EndGameScript : MonoBehaviour
+{
+
+    public void Load(string e)
+    {
+        SceneManager.LoadScene(e);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if ((transform.position.x >= 646 && transform.position.x <= 894) &&
+            (transform.position.y <= 129 && transform.position.y >= 83) &&
+            (transform.position.z <= -317 && transform.position.z >= -435))
+        {
+            Load("TheEnd");
+        }
+    }
+}
